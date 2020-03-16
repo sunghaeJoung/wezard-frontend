b0ab8a4aee71c9ff4eed33c5b8d99a77086eaf1a
dasoljong92 <56146490+dasoljong92@users.noreply.github.com>
2020-03-16 22:04:19 +0900

    news page 무한 스크롤 구현

--- src/Page/NewsFeatures/Bottom/Bottom.js
+++ src/Page/NewsFeatures/Bottom/Bottom.js
@@ -7,24 +7,134 @@ class Bottom extends Component {
     super();
 
     this.state = {
-      BottomData: []
+      items: 6,
+      preItems: 0,
+      preCoverUrl: "",
+      coverurl: "",
+      bottomData: []
     };
   }
 
-  componentDidMount() {
-    // fetch("http://localhost:3000/data/LatestNewsData.json", {
-    //   method: "GET"
-    // })
+  getItem = () => {
     fetch("http://localhost:3000/data/BottomData.json")
       .then(res => res.json())
       .then(res => {
+        console.log(res.BottomData);
+        let result = res.BottomData.slice(
+          this.state.preItems,
+          this.state.items
+        );
+        console.log(result);
         this.setState({
-          BottomData: res.BottomData
+          bottomData: result
         });
+        // this._getUrls();
+        // window.addEventListener("scroll", this._infiniteScroll, true);
       });
+  };
+
+  _infiniteScroll = () => {
+    let scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight
+    );
+    let scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    );
+    let clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight === scrollHeight) {
+      this.setState({
+        perItems: this.state.items,
+        items: this.state.items + 6
+      });
+
+      this.getItem();
+    }
+  };
+
+  componentDidMount() {
+    this.getItem();
+    window.addEventListener("scroll", this._infiniteScroll, true);
   }
 
+  // getItemApi = () => {
+  //   console.log("아이템");
+  //   fetch("http://localhost:3000/data/BottomData.json")
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       console.log(res.BottomData);
+  //       let result = res.BottomData.slice(
+  //         this.state.preItems,
+  //         this.state.items
+  //       );
+  //       console.log(result);
+  //       return result;
+  //     });
+  // };
+
+  // 무한루프 구현
+  // const [unit, setUnit] = useState({
+  //   items: 5,
+  //   preItems: 0,
+  //   scrolling: false,
+  //   bottomData: []
+  // });
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", infiniteScroll, true);
+  //   // getItem();
+  //   return () => window.removeEventListener("scroll", infiniteScroll);
+  // }, [unit]);
+
+  // const getItem = () => {
+  //   console.log("아이템");
+  //   fetch("http://localhost:3000/data/BottomData.json")
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setUnit({
+  //         ...unit,
+  //         bottomData: unit.bottomData.concat(
+  //           res.BottomData.slice(unit.preItems, unit.items)
+  //         ),
+  //         scrolling: !unit.scrolling
+  //       });
+  //     })
+  //     .then(res => console.log(unit.bottomData));
+  // };
+
+  // const infiniteScroll = () => {
+  //   console.log("스크롤");
+  //   let scroolHeight = Math.max(document.documentElement.scrollHeight);
+  //   let scrollTop = Math.max(document.documentElement.scrollTop);
+  //   let clientHeight = document.documentElement.clientHeight;
+  //   if (scrollTop + clientHeight + 1 > scroolHeight) {
+  //     setUnit({
+  //       ...unit,
+  //       preItems: unit.items,
+  //       items: unit.items + 5,
+  //       scrolling: !unit.scrolling
+  //     });
+  //     getItem();
+  //   }
+  // };
+
+  // mock 데이터 활용하여 데이터 받아 올 때
+  // componentDidMount() {
+  //   // fetch("http://localhost:3000/data/LatestNewsData.json", {
+  //   //   method: "GET"
+  //   // })
+  //   fetch("http://localhost:3000/data/BottomData.json")
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       this.setState({
+  //         BottomData: res.BottomData
+  //       });
+  //     });
+  // }
   render() {
+    console.log("데이터", this.state.bottomData);
     return (
       <BottomWrapper>
         <BodyWrapper>
@@ -32,7 +142,7 @@ class Bottom extends Component {
             <Head>More to explore</Head>
           </TitleWrapper>
           <WholeCardWrapper>
-            {this.state.BottomData.map(item => {
+            {this.state.bottomData.map(item => {
               return (
                 <BottomBox
                   thumbnail={item.thumbnail}
